8d4b23846913dc7ba14d80a10bed3d43f8af4d41
Aman Mayank <62825517+AmanMayank@users.noreply.github.com>
2024-04-10 10:06:26 +0530

    UserProgress with error Handling

--- db/queries.ts
+++ db/queries.ts
@@ -2,12 +2,7 @@ import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs";
 import { eq } from "drizzle-orm";
-import { userProgress } from "./schema";
-
-export const getCourses = cache(async () => {
-  const data = await db.query.courses.findMany();
-  return data;
-});
+import { courses, userProgress } from "@/db/schema";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -22,5 +17,18 @@ export const getUserProgress = cache(async () => {
       activeCourse: true,
     },
   });
+  return data;
+});
+
+export const getCourses = cache(async () => {
+  const data = await db.query.courses.findMany();
+  return data;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+  });
+
   return data;
 });
